73726b71e0322372663dcdd24e5d5256dc3b5c57
zDein <carlosantonioeyes@gmail.com>
2025-10-15 01:41:42 -0300

    create: aplicativo mobile

---
+++ aplicativo-hemograma/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+
+export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title={`Contador = ${count}`} onPress={() => setCount(count + 1)}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
